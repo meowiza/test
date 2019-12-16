9dc1bebda0cf9703f2bd3127a29f09f31c21c6c3
Luiza <luiza.khafizova@yahoo.com>
2019-12-15 21:26:22 -0800

    first commit

---
+++ data-base.js
@@ -0,0 +1,14 @@
+const dataBase = [
+  {
+    name: 'Bla Bla',
+    age: 0,
+    race: 'white'
+  },
+  {
+    name: 'Nick',
+    age: 5,
+    race: 'latino'
+  }
+];
+
+module.exports = dataBase
---
+++ index.js
@@ -0,0 +1,7 @@
+const db = require('./data-base.js');
+
+function printNames(array) {
+  array.forEach(el => console.log(el.name));
+}
+printNames(db);
+
